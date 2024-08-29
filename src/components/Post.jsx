34274d5b1db6a26047f6aec526003948f248aaaa
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-08-29 16:18:05 -0300

    Post component done

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/caio-guimaraes-web.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Caio Guimarães</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          title="29 de Agosto de 2024 às 11:00h"
+          dateTime="2024-29-08 11:00:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto </a>
+          <a href="">#nlw </a>
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
