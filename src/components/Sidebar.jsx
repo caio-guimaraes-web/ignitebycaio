1ab7ea966d833c8dcf69aa69276d57a146775172
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-09-17 10:05:22 -0300

    avatar component done and imported in other pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { PencilLine } from "phosphor-react"
-
+import { Avatar } from "./Avatar"
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -11,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/caio-guimaraes-web.png"
-        />
+        <Avatar src="https://github.com/caio-guimaraes-web.png" alt="" />
 
         <strong>Caio Guimarães</strong>
         <span>Web Developer</span>
