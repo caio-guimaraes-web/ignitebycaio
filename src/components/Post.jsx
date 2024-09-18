0bf76d1f4a13f8451ca16add6b50c709fc0f0271
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-09-18 17:52:46 -0300

    starting state on comment post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from "date-fns"
 import ptBR from "date-fns/locale/pt-BR"
+import { useState } from "react"
 
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
@@ -7,6 +8,7 @@ import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2])
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -20,6 +22,10 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   })
 
+  function handleCrateNewComment() {
+    event.preventDefault()
+    setComments([...comments, comments.length + 1])
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -53,17 +59,20 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+
         <textarea placeholder="Deixe um comentário" />
+
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
